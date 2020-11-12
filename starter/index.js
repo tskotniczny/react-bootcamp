0e5342c9711472802b1eca9563d4b7ada0649bd0
Tomek <tskotniczny1@gmail.com>
2020-11-12 19:58:39 +0000

    Add parcel bundler and run project

--- starter/index.js
+++ starter/index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // const element = React.createElement("div", null, "React element!");
 
 const element =<div> React element with JSX</div>
