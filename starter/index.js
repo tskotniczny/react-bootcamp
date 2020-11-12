8dbbb26f69dc8ff7dae386cc06a5ab86e8a8fb82
Tomek <tskotniczny1@gmail.com>
2020-11-12 19:45:39 +0000

    Add react & parcel bundler to create react app from scratch

---
+++ starter/index.js
@@ -0,0 +1,5 @@
+// const element = React.createElement("div", null, "React element!");
+
+const element =<div> React element with JSX</div>
+
+ReactDOM.render(element, document.getElementById("root"));
