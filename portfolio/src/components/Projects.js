64eeb2163fdd59c10ca163e6ad2963a29eedcf0e
Tomek <tskotniczny1@gmail.com>
2020-11-15 00:56:17 +0000

    Refactor components to stateless components where possible

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,41 +1,32 @@
-import React, { Component } from "react";
+import React from "react"; 
 import PROJECTS from "../data/projects";
 
-class Project extends Component {
-    render(){
-        console.log("this props", this.props);
+const Project = props => {
+    const {title, image, description, link} = props.project;
 
-        const {title, image, description, link} = this.props.project;
-
-        return(
-            <div style={{display: "inline-block", width: 300, margin: 10}}> 
-                <h3>{title}</h3>
-                <img src={image} alt="profile" style={{width: 200, height: 120 }}/>
-                <p>{description}</p>
-                <a href={link}>{link}</a>
-            </div>
-        );
-    }
+    return(
+        <div style={{display: "inline-block", width: 300, margin: 10}}> 
+            <h3>{title}</h3>
+            <img src={image} alt="profile" style={{width: 200, height: 120 }}/>
+            <p>{description}</p>
+            <a href={link}>{link}</a>
+        </div>
+    )
 }
 
-class Projects extends Component {
-    render(){
-        return (
-            <div>
-                <h2> Highlighted projects</h2>
-                <div>
-                    {
-                        PROJECTS.map(PROJECT => {
-                                return (
-                                    <Project key={PROJECT.id} project={PROJECT}/>
-                                );
-                            }
-                        )
-                    }
-                </div>
-            </div>
-        )
-    }
-}
+
+const Projects = () => (
+    <div>
+        <h2> Highlighted projects</h2>
+        <div>
+            {
+                PROJECTS.map(PROJECT => (
+                        <Project key={PROJECT.id} project={PROJECT}/>
+                    )
+                )
+            }
+        </div>
+    </div>
+)
 
 export default Projects;
--- portfolio/src/components/SocialProfiles.js
+++ portfolio/src/components/SocialProfiles.js
@@ -1,39 +1,30 @@
-import React, { Component } from "react";
+import React from "react"; //import of { component } can be deleted when using stateless components 
 import SOCIAL_PROFILES from "../data/socialProfiles";
 
+//Stateless component, which means it can now have an input of "props" which means I can delete "this.props.social_profile" and change it to the line below
+const SocialProfile = props => {
+    const {link, image} = props.social_profile;
 
-class SocialProfile extends Component{
-    render(){
-
-        const {link, image} = this.props.social_profile;
-
-        return(
-            <span style={{width: 35, height: 35, margin: 10}}>
-                <a href={link}><img src={image} alt="social-profile" style={{width: 50, height: 50}}/></a>
-            </span>
-        );
-    }
+    return(
+        <span style={{width: 35, height: 35, margin: 10}}>
+            <a href={link}><img src={image} alt="social-profile" style={{width: 50, height: 50}}/></a>
+        </span>
+    );
 }
 
-
-class SocialProfiles extends Component{
-    render(){
-        return(
-        <div>
-            <h3>Social profiles</h3>
-            <div> 
-                {
-                    SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
-                            return(
-                                <SocialProfile key={SOCIAL_PROFILE.id} social_profile={SOCIAL_PROFILE}/>
-                            );
-                        }
+//inline return (just return os simpliefied it and deleted it and changed {} to ())
+const SocialProfiles = () => (
+    <div>
+        <h3>Social profiles</h3>
+        <div> 
+            {
+                SOCIAL_PROFILES.map(SOCIAL_PROFILE => (
+                        <SocialProfile key={SOCIAL_PROFILE.id} social_profile={SOCIAL_PROFILE}/>
                     )
-                }
-            </div>
+                )
+            }
         </div>
-        )
-    }
-}
+    </div>
+)
 
 export default SocialProfiles;
