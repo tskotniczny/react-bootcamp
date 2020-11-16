fdeaddb2a863cd68cdfed0bb3197bf62e03d4baa
Tomek <tskotniczny1@gmail.com>
2020-11-16 18:51:15 +0000

    Error handling in jokes fetch api call

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,3 +4,20 @@ import App from './components/App';
 import "./index.css";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+
+
+//Promise and how it works in the background
+// new Promise((resolve, reject) => {
+//     return reject(new Error("No Bears"));
+
+//     setTimeout(() => {
+//         resolve("Bears, Beets, Battlestar Galactica");
+//     }, 2000);
+// })
+// .then( quote => {
+//     console.log(quote);
+// })
+// .catch (error => console.log("Error", error));
+
+
